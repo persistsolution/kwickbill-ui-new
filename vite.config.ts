38a80855efd7e33614bf6c68c0c13a3044269d81
persistsolution <persistsolution1@gmail.com>
2025-01-18 21:12:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isMainBranch = process.env.GITHUB_REF_NAME === 'main';
+export default defineConfig({
 
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': {}, // Prevent issues with process.env in the browser.
-    },
-    base: isMainBranch ? '/' : '/dev/', // Base URL for assets. Adjust as per deployment needs.
-    build: {
-      outDir: isMainBranch ? 'build-main' : 'build-dev', // Output directory for the build.
-      sourcemap: true, // Optional: Enables source maps for easier debugging.
-    },
-    server: {
-      host: true, // Allows external connections.
-      port: 5173, // Default port.
-    },
-  };
-});
+  // base: "/vexel-ts/preview/", Use base path for while deploying the project the SSR.
+  
+  plugins: [react()],
+  define: {
+    'process.env': {}
+  },
+  build: {
+    chunkSizeWarningLimit: 50000,
+    minify: true,
+  },
+  server: {
+    host: true,
+    port: 5175, // Default port
+  }
+})
