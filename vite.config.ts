3047aa4de762b20669f6f9627047026499043321
persistsolution <persistsolution1@gmail.com>
2025-01-19 00:34:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  // base: "/vexel-ts/preview/", Use base path for while deploying the project the SSR.
-  
   plugins: [react()],
   define: {
-    'process.env': {}
-  },
-  build: {
-
-    chunkSizeWarningLimit: 50000,
-    minify: true,
+    'process.env': {},
   },
   server: {
-    host: true,
-    port: 5173, // Default port
-  }
-})
+    host: '0.0.0.0', // Bind to all IP addresses
+    port: 5173, // Set the port
+    strictPort: true, // Ensure the server uses the specified port or fails
+  },
+  build: {
+    outDir: 'dist', // Custom output directory for builds
+    sourcemap: true, // Include source maps for debugging
+  },
+});
